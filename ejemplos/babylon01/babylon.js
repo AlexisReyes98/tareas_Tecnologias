4f9d76ad1419007c9ae15a0fa75c1fabeb685c6f
Alexis Reyes <giovanny29.garv@gmail.com>
2023-03-05 22:54:09 -0600

    iniciando babylon

--- ejemplos/babylon01/babylon.js
+++
@@ -1,9 +0,0 @@
-var createScene  = function() {
-    var scene = new BABYLON.Scene(engine);
-    scene.clearColor = new BABYLON.Color3(1, 0.8, 0.8);
-};
-var scene = createScene();
-
-engine.runRenderLoop(function() {
-    scene.render();
-});
